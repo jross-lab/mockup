6a92d24249a2e4d624c6f4bf8e8cc4b42670a826
jross-lab <jross@strava.com>
2026-03-20 15:44:16 +0000

    Add minified React application code

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Minified React application code extracted from index.html
+
+// Assuming the minified code looks something like this:
+console.log('Hello, world!');
+// ... (the rest of the minified code) ...
