5ab2ccb70c9cf089f5981843f21c3b26cdb871d5
jross-lab <jross@strava.com>
2026-03-20 17:17:45 +0000

    Delete js directory

--- js/app.js
+++
@@ -1,5 +0,0 @@
-// Minified React application code extracted from index.html
-
-// Assuming the minified code looks something like this:
-console.log('Hello, world!');
-// ... (the rest of the minified code) ...
