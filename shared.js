93bf51ed3819e030d39a6d2c8b197e0436fce296
jross-lab <jross@strava.com>
2026-03-23 10:21:09 +0000

    Top Nav

--- shared.js
+++ shared.js
@@ -64,7 +64,7 @@ function TopNav({ title = "Challenge", back = "Groups" }) {
     </div>
   );
 }
-// TopNavHome — matches Figma node 2119:22942: status bar + "Home" title + 4 icons + drop shadow
+// TopNavHome — renders the exact top nav SVG provided, with status bar above
 function TopNavHome() {
   return (
     <div style={{ background: T.bgSurface, display: "flex", flexDirection: "column", boxShadow: "0px 2px 4px rgba(0,0,0,0.1)", flexShrink: 0 }}>
@@ -75,23 +75,23 @@ function TopNavHome() {
           <IcoCellular/><IcoWifi/><IcoBattery/>
         </div>
       </div>
-      {/* Title and Controls row */}
-      <div style={{ height: 46, display: "flex", alignItems: "center", position: "relative" }}>
-        {/* Leading icons */}
-        <div style={{ position: "absolute", left: 0, display: "flex", alignItems: "center", gap: 16, paddingLeft: 24, height: "100%" }}>
-          <div style={{ width: 24, height: 24, display: "flex", alignItems: "center", justifyContent: "center" }}><IcoViewfinder/></div>
-          <div style={{ width: 24, height: 24, display: "flex", alignItems: "center", justifyContent: "center" }}><IcoSearch/></div>
-        </div>
-        {/* Center title */}
-        <div style={{ flex: 1, textAlign: "center", paddingLeft: 82, paddingRight: 82 }}>
-          <span style={{ fontFamily: T.font, fontSize: 17, fontWeight: 700, lineHeight: "22px", color: T.textPri }}>Home</span>
-        </div>
-        {/* Trailing icons */}
-        <div style={{ position: "absolute", right: 0, display: "flex", alignItems: "center", gap: 16, paddingRight: 24, height: "100%" }}>
-          <div style={{ width: 24, height: 24, display: "flex", alignItems: "center", justifyContent: "center" }}><IcoMessaging/></div>
-          <div style={{ width: 24, height: 24, display: "flex", alignItems: "center", justifyContent: "center" }}><IcoNotifications/></div>
-        </div>
-      </div>
+      {/* Title and Controls row — pixel-perfect SVG from Figma */}
+      <svg width="375" height="48" viewBox="0 0 375 48" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ display: "block", flexShrink: 0 }}>
+        <g clipPath="url(#tnClip0)">
+          <path d="M37 17V23H43V25H37V31H35V25H29V23H35V17H37Z" fill="black"/>
+          <path d="M24 24C24 17.3726 29.3726 12 36 12C42.6274 12 48 17.3726 48 24C48 30.6274 42.6274 36 36 36C29.3726 36 24 30.6274 24 24ZM36 14C30.4772 14 26 18.4772 26 24C26 29.5228 30.4772 34 36 34C41.5228 34 46 29.5228 46 24C46 18.4772 41.5228 14 36 14Z" fill="black"/>
+        </g>
+        <path d="M73.4571 12C78.1515 12 81.9571 15.8056 81.9571 20.5C81.9571 21.9692 81.5844 23.3513 80.9283 24.557L87.5607 31.1893C88.1464 31.7751 88.1464 32.7249 87.5607 33.3107L86.2678 34.6036C85.682 35.1893 84.7322 35.1893 84.1464 34.6036L77.5141 27.9712C76.3084 28.6273 74.9263 29 73.4571 29C68.7627 29 64.9571 25.1944 64.9571 20.5C64.9571 15.8056 68.7627 12 73.4571 12ZM79.9571 20.5C79.9571 16.9101 77.047 14 73.4571 14C69.8673 14 66.9571 16.9101 66.9571 20.5C66.9571 24.0899 69.8673 27 73.4571 27C77.047 27 79.9571 24.0899 79.9571 20.5ZM79.1675 26.7962L85.2071 32.8358L85.7929 32.25L79.7533 26.2104C79.5676 26.415 79.3721 26.6105 79.1675 26.7962Z" fill="black"/>
+        <path d="M167.013 29V17.015H169.478V21.69H174.68V17.015H177.145V29H174.68V23.866H169.478V29H167.013ZM183.101 29.255C180.466 29.255 178.834 27.351 178.834 24.614C178.834 21.877 180.466 19.99 183.101 19.99C185.719 19.99 187.368 21.877 187.368 24.614C187.368 27.351 185.719 29.255 183.101 29.255ZM183.101 27.3C184.223 27.3 184.954 26.331 184.954 24.614C184.954 22.897 184.223 21.945 183.101 21.945C181.962 21.945 181.231 22.897 181.231 24.614C181.231 26.331 181.962 27.3 183.101 27.3ZM188.922 29V20.245H191.166V21.248H191.234C191.676 20.449 192.509 19.99 193.495 19.99C194.566 19.99 195.382 20.466 195.824 21.35H195.892C196.385 20.483 197.286 19.99 198.306 19.99C200.091 19.99 201.077 21.129 201.06 23.186V29H198.714V23.305C198.714 22.387 198.323 21.945 197.575 21.945C196.98 21.945 196.453 22.336 196.164 22.982V29H193.818V23.305C193.818 22.387 193.427 21.945 192.679 21.945C192.084 21.945 191.557 22.319 191.268 22.965V29H188.922ZM206.79 29.255C204.172 29.255 202.608 27.487 202.608 24.597C202.608 21.809 204.274 19.99 206.671 19.99C209.102 19.99 210.734 21.707 210.734 24.24C210.734 24.631 210.7 24.988 210.649 25.277H205.022C205.039 26.739 205.719 27.453 206.79 27.453C207.538 27.453 207.997 27.079 208.235 26.382L210.513 26.96C210.071 28.371 208.711 29.255 206.79 29.255ZM205.022 23.815H208.371C208.337 22.523 207.793 21.707 206.722 21.707C205.685 21.707 205.039 22.37 205.022 23.815Z" fill="black"/>
+        <g clipPath="url(#tnClip1)">
+          <path d="M304 19.0571V16.0571C304 14.4003 302.657 13.0571 301 13.0571H290C288.343 13.0571 287 14.4003 287 16.0571V23.0571C287 24.3633 287.835 25.4746 289 25.8864V28.2465C289 29.8056 290.885 30.5863 291.987 29.4839L294 27.4713V29.0571C294 30.714 295.343 32.0571 297 32.0571H302.586L306.013 35.4839C307.115 36.5863 309 35.8056 309 34.2465V31.8864C310.165 31.4746 311 30.3633 311 29.0571V22.0571C311 20.4003 309.657 19.0571 308 19.0571H304ZM290 15.0571H301C301.552 15.0571 302 15.5048 302 16.0571V19.0571H297C295.343 19.0571 294 20.4003 294 22.0571V24.6429L291 27.6429V24.0571H290C289.448 24.0571 289 23.6094 289 23.0571V16.0571C289 15.5048 289.448 15.0571 290 15.0571ZM309 22.0571V29.0571C309 29.6094 308.552 30.0571 308 30.0571H307V33.6429L303.414 30.0571H297C296.448 30.0571 296 29.6094 296 29.0571V22.0571C296 21.5048 296.448 21.0571 297 21.0571H308C308.552 21.0571 309 21.5048 309 22.0571Z" fill="black"/>
+        </g>
+        <path d="M339 12C334.582 12 331 15.5817 331 20V23.5858L328.586 26C328.211 26.3751 328 26.8838 328 27.4142V28C328 29.1046 328.895 30 330 30H334C334 32.7614 336.239 35 339 35C341.761 35 344 32.7614 344 30H348C349.105 30 350 29.1046 350 28V27.4142C350 26.8838 349.789 26.3751 349.414 26L347 23.5858V20C347 15.5817 343.418 12 339 12ZM333 20C333 16.6863 335.686 14 339 14C342.314 14 345 16.6863 345 20V24.4142L348 27.4142V28H330V27.4142L333 24.4142V20ZM342 30C342 31.6569 340.657 33 339 33C337.343 33 336 31.6569 336 30H342Z" fill="black"/>
+        <defs>
+          <clipPath id="tnClip0"><rect width="24" height="24" fill="white" transform="translate(24 12)"/></clipPath>
+          <clipPath id="tnClip1"><rect width="24" height="24" fill="white" transform="translate(287 12)"/></clipPath>
+        </defs>
+      </svg>
     </div>
   );
 }
